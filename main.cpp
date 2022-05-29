cb52769ea94cdc8bf562fa304599c3d3ec606156
Derek Zhang <45522063+DerekZhang0000@users.noreply.github.com>
2022-05-28 21:45:56 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,77 +1,74 @@
 #include <opencv2/opencv.hpp>
 
-void normFunction(cv::Mat &image, double &recolorFactor, double &threshold, unsigned int i, unsigned int j, bool ignoreMonochrome)
+using namespace std;
+using namespace cv;
+#define PIXEL pair<const unsigned int, const unsigned int>
+
+bool isMonochrome(double ignoreMonochrome, double subColor1, double subColor2, double subColor3)
 {
-    cv::Vec3b color = image.at<cv::Vec3b>(cv::Point(i, j));
-    int red = color.val[2]; //Red
-    int green = color.val[1];   //Green
-    int blue = color.val[0];    //Blue
-    if (ignoreMonochrome == true)
+    if (abs(subColor1 - subColor2) / 255 <= ignoreMonochrome && abs(subColor2 - subColor3) / 255 <= ignoreMonochrome && abs(subColor3 - subColor1) / 255 <= ignoreMonochrome)
     {
-        if (red == green && green == blue)
+        return true;
+    }
+    return false;
+}
+void normFunction(Mat &image, double recolorFactor, PIXEL pixel, double threshold, string gbDecay, double ignoreMonochrome)
+{
+    Vec3b color = image.at<Vec3b>(Point(pixel.first, pixel.second));
+    unsigned char red = color.val[2];       //Red
+    unsigned char green = color.val[1];     //Green
+    unsigned char blue = color.val[0];      //Blue
+    if (ignoreMonochrome > 0 && ignoreMonochrome < 1)
+    {
+        if (isMonochrome(1 - ignoreMonochrome, red, green, blue))
         {
             return;
         }
     }
     if (abs(red - green) / 255 < threshold)
     {
-        double rValue = red * (1 + recolorFactor);
-        if (rValue > 255.0)
+        unsigned int rValue = round(red * (1 + recolorFactor));
+        if (rValue > 255)
         {
-            image.at<cv::Vec3b>(cv::Point(i, j))[2] = (int)rValue;
-            image.at<cv::Vec3b>(cv::Point(i, j))[1] = (int)(green - 255 * pow(((rValue - 255.0) / 255.0), 2.0));
+            int gValue = gbDecay == "sqr_diff" ? round((green - 255 * pow(((rValue - 255.0) / 255), 2.0))) : green - (rValue - 255);
+            image.at<Vec3b>(Point(pixel.first, pixel.second))[2] = 255;
+            if (gValue < 0)
+            {
+                int bValue = gbDecay == "sqr_diff" ? round((blue - 255 * pow((-gValue / 255), 2.0))) : blue + gValue;
+                image.at<Vec3b>(Point(pixel.first, pixel.second))[1] = 0;
+                image.at<Vec3b>(Point(pixel.first, pixel.second))[0] = bValue < 0 ? 0 : bValue;
+            }
+            else
+            {
+                image.at<Vec3b>(Point(pixel.first, pixel.second))[1] = gValue;
+            }
         }
         else
         {
-            image.at<cv::Vec3b>(cv::Point(i, j))[2] = (int)rValue;
+            image.at<Vec3b>(Point(pixel.first, pixel.second))[2] = rValue;
         }
     }
 }
-void grayFunction(cv::Mat &image, unsigned int i, unsigned int j)
+void normalizeColors(Mat &image, double recolorFactor=.5, double threshold=1, string gbDecay="lin_diff", double ignoreMonochrome=-1)
 {
-    cv::Vec3b color = image.at<cv::Vec3b>(cv::Point(i, j));
-    int red = color.val[2]; //Red
-    int green = color.val[1];   //Green
-    int blue = color.val[0];    //Blue
-    if (red > green)
+    const unsigned int imageWidth = image.cols;
+    const unsigned int imageHeight = image.rows;
+    for (unsigned int i = 0; i < imageWidth; i++)
     {
-        image.at<cv::Vec3b>(cv::Point(i, j))[2] = 0;
-        image.at<cv::Vec3b>(cv::Point(i, j))[1] = 0;
-        image.at<cv::Vec3b>(cv::Point(i, j))[0] = 0;
-    }
-    else if (green > red)
-    {
-        image.at<cv::Vec3b>(cv::Point(i, j))[2] = 200;
-        image.at<cv::Vec3b>(cv::Point(i, j))[1] = 200;
-        image.at<cv::Vec3b>(cv::Point(i, j))[0] = 200;
-    }
-    else if (!(red == green && green == blue))
-    {
-        image.at<cv::Vec3b>(cv::Point(i, j))[2] = 100;
-        image.at<cv::Vec3b>(cv::Point(i, j))[1] = 100;
-        image.at<cv::Vec3b>(cv::Point(i, j))[0] = 100;
-    }
-}
-void normalizeColors(cv::Mat &image, double recolorFactor, double threshold, bool ignoreMonochrome = false)
-{
-    assert(image.cols > 0 && recolorFactor >= 0 && threshold >= 0);
-    int imageWidth = image.cols;
-    int imageHeight = image.rows;
-    for (int i = 0; i < imageWidth; i++)
-    {
-        for (int j = 0; j < imageHeight; j++)
+        for (unsigned int j = 0; j < imageHeight; j++)
         {
-            normFunction(image, recolorFactor, threshold, i, j, ignoreMonochrome);
-            // grayFunction(image, i, j);
+            normFunction(image, recolorFactor, PIXEL(i, j), threshold, gbDecay, ignoreMonochrome);
         }
     }
 }
 int main() {
-
-    cv::Mat image = cv::imread("../../ImageIn/cbTests.jpg", cv::IMREAD_COLOR);
-    normalizeColors(image, .50, .5, false);
-    cv::imshow("Output", image);
-    cv::imwrite("../../ImageOut/cbTests.jpg", image);
-    int i = cv::waitKey(0);
+    Mat image = imread("../../ImageIn/cbTests.jpg", IMREAD_COLOR);
+    // resize(image, image, Size(1800, 1080), INTER_LINEAR);
+    normalizeColors(image, .5, 1, "sqr_diff", .9);
+    imshow("Output", image);
+    imwrite("../../ImageOut/cbTests.jpg", image);
+    int i = waitKey(0);
     return 0;
 }
+
+#undef PIXEL
