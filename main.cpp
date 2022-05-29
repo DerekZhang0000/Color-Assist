df921869041de4c9f54c19bf90643f24de29d551
Derek Zhang <45522063+DerekZhang0000@users.noreply.github.com>
2022-05-28 22:05:13 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ bool isMonochrome(double ignoreMonochrome, double subColor1, double subColor2, d
     }
     return false;
 }
-void normFunction(Mat &image, double recolorFactor, PIXEL pixel, double threshold, string gbDecay, double ignoreMonochrome)
+void normFunction(Mat &image, double recolorFactor, PIXEL pixel, double ignoreMonochrome)
 {
     Vec3b color = image.at<Vec3b>(Point(pixel.first, pixel.second));
     unsigned char red = color.val[2];       //Red
@@ -25,31 +25,28 @@ void normFunction(Mat &image, double recolorFactor, PIXEL pixel, double threshol
             return;
         }
     }
-    if (abs(red - green) / 255 < 1 - threshold)
+    unsigned int rValue = round(red * (1 + recolorFactor));
+    if (rValue > 255)
     {
-        unsigned int rValue = round(red * (1 + recolorFactor));
-        if (rValue > 255)
+        int gValue = green - (rValue - 255);
+        image.at<Vec3b>(Point(pixel.first, pixel.second))[2] = 255;
+        if (gValue < 0)
         {
-            int gValue = gbDecay == "sqr_diff" ? round((green - 255 * pow(((rValue - 255.0) / 255), 2.0))) : green - (rValue - 255);
-            image.at<Vec3b>(Point(pixel.first, pixel.second))[2] = 255;
-            if (gValue < 0)
-            {
-                int bValue = gbDecay == "sqr_diff" ? round((blue - 255 * pow((-gValue / 255), 2.0))) : blue + gValue;
-                image.at<Vec3b>(Point(pixel.first, pixel.second))[1] = 0;
-                image.at<Vec3b>(Point(pixel.first, pixel.second))[0] = bValue < 0 ? 0 : bValue;
-            }
-            else
-            {
-                image.at<Vec3b>(Point(pixel.first, pixel.second))[1] = gValue;
-            }
+            int bValue = blue + gValue;
+            image.at<Vec3b>(Point(pixel.first, pixel.second))[1] = 0;
+            image.at<Vec3b>(Point(pixel.first, pixel.second))[0] = bValue < 0 ? 0 : bValue;
         }
         else
         {
-            image.at<Vec3b>(Point(pixel.first, pixel.second))[2] = rValue;
+            image.at<Vec3b>(Point(pixel.first, pixel.second))[1] = gValue;
         }
     }
+    else
+    {
+        image.at<Vec3b>(Point(pixel.first, pixel.second))[2] = rValue;
+    }
 }
-void normalizeColors(Mat &image, double recolorFactor=.5, double threshold=1, string gbDecay="lin_diff", double ignoreMonochrome=-1)
+void normalizeColors(Mat &image, double recolorFactor=.5, double ignoreMonochrome=-1)
 {
     const unsigned int imageWidth = image.cols;
     const unsigned int imageHeight = image.rows;
@@ -57,14 +54,14 @@ void normalizeColors(Mat &image, double recolorFactor=.5, double threshold=1, st
     {
         for (unsigned int j = 0; j < imageHeight; j++)
         {
-            normFunction(image, recolorFactor, PIXEL(i, j), threshold, gbDecay, ignoreMonochrome);
+            normFunction(image, recolorFactor, PIXEL(i, j), ignoreMonochrome);
         }
     }
 }
 int main() {
     Mat image = imread("../../ImageIn/cbTests.jpg", IMREAD_COLOR);
     // resize(image, image, Size(1260, 700), INTER_LINEAR);
-    normalizeColors(image, .5, 0, "sqr_diff", .9);
+    normalizeColors(image, .35, .9);
     imshow("Output", image);
     // imwrite("../../ImageOut/cbTests.jpg", image);
     int i = waitKey(0);
